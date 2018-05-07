c04d6756ce690b36555d4f66fe3fa52cf0f0a1d1
Sebastian Bliefert <sebastian.bliefert@dfki.de>
2018-05-08 00:07:08 +0200

    add cyrillic support

--- badges.py
+++ badges.py
@@ -15,11 +15,17 @@ def escape(str):
     return str
 
 
+def getTextWidthMul(text, max_width):
+    return 1 if max_width > text.bbox().width() \
+             else max_width / unit.tocm(text.bbox().width())
+
+
 class Badge:
     height = 74                     # Do not change this, just used for formatting
     width = 105                     # Do not change this, just used for formatting
-    font = "cabin"                  # Take a font known by your LaTeX distribution (e.g. texlive)
+    font_module = "cabin"           # Take a font known by your LaTeX distribution (e.g. texlive)
     font_attr = "sfdefault"         # optional attributes for your font
+    font_family = "Cabin-TLF"
     logo_color = color.grey(0.7)    # Fill color of your logo (just needed for some logos)
     tmp_dir = "tmp"                 # temp directory to be created and deleted while generating pdf
 
@@ -37,13 +43,12 @@ class Badge:
         framesize = 0
         margin = 3
         name_height = 10
-        name_size = 370
-        name_width = 62
-        handle_height = name_height + (name_height * 0.5) + 5
-        handle_size = name_size * 0.5
-        uni_height = handle_height + (name_height * 0.375) + 3
-        uni_size = name_size * 0.375
-        kif_size = name_size * 0.75
+        handle_height = 5
+        uni_height = 3.75
+        kif_height = 7.5
+        txt_width = 62
+        handle_pos = name_height + handle_height + 5
+        uni_pos = handle_pos + uni_height + 3
         qr_size = 40
         c = canvas.canvas()
 
@@ -54,71 +59,88 @@ class Badge:
                                  path.lineto(0, self.profile.height),
                                  path.closepath())
         # create frame
-        frame = path.path(path.moveto(self.x(framesize), self.y(framesize)),
-                          path.lineto(self.profile.width - self.x(framesize), self.y(framesize)),
-                          path.lineto(self.profile.width - self.x(framesize), self.profile.height - self.y(framesize)),
-                          path.lineto(self.x(framesize), self.profile.height - self.y(framesize)),
+        frame = path.path(path.moveto(framesize, (framesize)),
+                          path.lineto(self.profile.width - framesize, (framesize)),
+                          path.lineto(self.profile.width - framesize, self.profile.height - (framesize)),
+                          path.lineto(framesize, self.profile.height - (framesize)),
                           path.closepath())
 
-        sticker = path.path(path.moveto(self.profile.width - self.x(framesize+margin),
-                                        self.profile.height - self.y(framesize+margin)),
-                            path.lineto(self.profile.width - self.x(framesize+margin),
-                                        self.profile.height - self.y(framesize+margin+self.profile.sticker_size)),
-                            path.lineto(self.profile.width - self.x(framesize+margin+self.profile.sticker_size),
-                                        self.profile.height - self.y(framesize+margin+self.profile.sticker_size)),
-                            path.lineto(self.profile.width - self.x(framesize+margin+self.profile.sticker_size),
-                                        self.profile.height - self.y(framesize+margin)),
-                            path.lineto(self.profile.width - self.x(framesize + margin),
-                                        self.profile.height - self.y(framesize + margin)),
-                            path.moveto(self.profile.width - self.x(framesize + margin),
-                                        self.profile.height - self.y(framesize + margin+ self.profile.sticker_size + 2)),
-                            path.lineto(self.profile.width - self.x(framesize + margin),
-                                        self.profile.height - self.y(framesize + margin + self.profile.sticker_size+self.profile.sticker_size + 2)),
-                            path.lineto(self.profile.width - self.x(framesize + margin + self.profile.sticker_size),
-                                        self.profile.height - self.y(framesize + margin + self.profile.sticker_size+self.profile.sticker_size + 2)),
-                            path.lineto(self.profile.width - self.x(framesize + margin + self.profile.sticker_size),
-                                        self.profile.height - self.y(framesize + margin+self.profile.sticker_size + 2)),
+        sticker = path.path(path.moveto(self.profile.width - (framesize+margin),
+                                        self.profile.height - (framesize+margin)),
+                            path.lineto(self.profile.width - (framesize+margin),
+                                        self.profile.height - (framesize+margin+self.profile.sticker_size)),
+                            path.lineto(self.profile.width - (framesize+margin+self.profile.sticker_size),
+                                        self.profile.height - (framesize+margin+self.profile.sticker_size)),
+                            path.lineto(self.profile.width - (framesize+margin+self.profile.sticker_size),
+                                        self.profile.height - (framesize+margin)),
+                            path.lineto(self.profile.width - (framesize + margin),
+                                        self.profile.height - (framesize + margin)),
+                            path.moveto(self.profile.width - (framesize + margin),
+                                        self.profile.height - (framesize + margin+ self.profile.sticker_size + 2)),
+                            path.lineto(self.profile.width - (framesize + margin),
+                                        self.profile.height - (framesize + margin + self.profile.sticker_size+self.profile.sticker_size + 2)),
+                            path.lineto(self.profile.width - (framesize + margin + self.profile.sticker_size),
+                                        self.profile.height - (framesize + margin + self.profile.sticker_size+self.profile.sticker_size + 2)),
+                            path.lineto(self.profile.width - (framesize + margin + self.profile.sticker_size),
+                                        self.profile.height - (framesize + margin+self.profile.sticker_size + 2)),
                             path.closepath())
 
-        points = (path.circle(self.profile.width-self.x(framesize+2*margin+self.profile.sticker_size+0.5*self.profile.point_size),
-                             self.profile.height - self.y(framesize + margin+0.5*self.profile.point_size),
-                             self.x(0.5*self.profile.point_size)),
-                  path.circle(self.profile.width - self.x(framesize + 2 * margin + self.profile.sticker_size + 0.5 * self.profile.point_size),
-                              self.profile.height - self.y(framesize + margin + self.profile.sticker_size + 1),
-                              self.x(0.5 * self.profile.point_size)),
-                  path.circle(self.profile.width - self.x(framesize + 2 * margin + self.profile.sticker_size + 0.5 * self.profile.point_size),
-                              self.profile.height - self.y(framesize + margin + 2 * self.profile.sticker_size + 2 - 0.5 * self.profile.point_size),
-                              self.x(0.5 * self.profile.point_size)))
+        points = (path.circle(self.profile.width-(framesize+2*margin+self.profile.sticker_size+0.5*self.profile.point_size),
+                             self.profile.height - (framesize + margin+0.5*self.profile.point_size),
+                             (0.5*self.profile.point_size)),
+                  path.circle(self.profile.width - (framesize + 2 * margin + self.profile.sticker_size + 0.5 * self.profile.point_size),
+                              self.profile.height - (framesize + margin + self.profile.sticker_size + 1),
+                              (0.5 * self.profile.point_size)),
+                  path.circle(self.profile.width - (framesize + 2 * margin + self.profile.sticker_size + 0.5 * self.profile.point_size),
+                              self.profile.height - (framesize + margin + 2 * self.profile.sticker_size + 2 - 0.5 * self.profile.point_size),
+                              (0.5 * self.profile.point_size)))
 
 
-        runner = text.latexrunner(texenc="utf8")
-        runner.preamble(r"\usepackage{lmodern} \usepackage[" + Badge.font_attr + r"]{" + Badge.font + r"} \usepackage[utf8]{inputenc}")
-        name = runner.text(0,0,
-                           r"{\fontsize{" + str(name_size) + "}{0} \selectfont " + self.name +"}")
-        handle = runner.text(self.x(framesize+margin),
-                             self.profile.height - self.y(framesize+margin+handle_height),
-                             r"{\fontsize{" + str(handle_size) + "}{0} \selectfont " + self.handle +"}")
-        uni = runner.text(self.x(framesize+margin),
-                          self.profile.height - self.y(framesize+margin+uni_height),
-                          r"{\fontsize{" + str(uni_size) + "}{0} \selectfont " + self.uni +"}")
-        kif = runner.text(self.x(framesize+margin),
-                          self.y(framesize+margin),
-                          r"{\fontsize{" + str(kif_size) + "}{0} \selectfont " + self.profile.conf_name +"}")
+        runner = text.latexrunner(texenc="utf-8", errordetail=text.errordetail.none)
+        runner.preamble(r"\usepackage{lmodern}")
+        runner.preamble(r"\usepackage[" + Badge.font_attr + r"]{" + Badge.font_module + r"}")
+        runner.preamble(r"\usepackage[utf8]{inputenc}")
+        runner.preamble(r"\usepackage[ngerman,russian]{babel}")
+        runner.preamble(r"\usepackage{etoolbox}")
+        runner.preamble(r"\preto\extrasrussian{\fontfamily{cmr}}")
+        runner.preamble(r"\preto\latintext{\fontfamily{" + Badge.font_family + r"}}")
+        runner.preamble(r"\AtBeginDocument{\selectlanguage{ngerman}}")
+
+        mul_txt = runner.text(0,0,"A")
+        txt_multiplier = 1 / unit.tocm(mul_txt.height)
+
+        name_txt = runner.text(0,0,self.name)
+        name = canvas.canvas()
+        name.insert(name_txt,[trafo.scale(txt_multiplier * name_height)])
+
+        handle_txt = runner.text(0,0,self.handle)
+        handle = canvas.canvas()
+        handle.insert(handle_txt, [trafo.scale(txt_multiplier * handle_height)])
+
+        uni_txt = runner.text(0,0,self.uni)
+        uni = canvas.canvas()
+        uni.insert(uni_txt, [trafo.scale(txt_multiplier * uni_height)])
+
+        kif_txt = runner.text(0,0,self.profile.conf_name)
+        kif = canvas.canvas()
+        kif.insert(kif_txt, [trafo.scale(txt_multiplier * kif_height),
+                                   trafo.translate((framesize+margin),
+                                                   (framesize+margin))])
 
         logo = canvas.canvas()
-        svg = svgfile.svgfile(self.x(framesize+self.profile.logo_offset[0]),
-                              self.y(framesize+self.profile.logo_offset[1]),
+        svg = svgfile.svgfile((framesize+self.profile.logo_offset[0]),
+                              (framesize+self.profile.logo_offset[1]),
                               self.profile.logo,
                               height=self.profile.logo_height,
                               parsed=True,
                               fill=Badge.logo_color)
         logo.insert(svg)
-        
+
 
         self.qr()
         qr = canvas.canvas()
-        qr.insert(svgfile.svgfile(self.width-self.x(framesize+qr_size),
-                                  self.y(framesize),
+        qr.insert(svgfile.svgfile(self.width-(framesize+qr_size),
+                                  (framesize),
                                   self.tmp_file,
                                   height=qr_size,
                                   parsed=True))
@@ -135,12 +157,15 @@ class Badge:
             for i in range(3):
                 c.stroke(points[i], [style.linewidth.THICk, style.linestyle.dotted])
 
-        namesmultiplier = 1 if self.x(name_width) > name.bbox().width() else self.x(name_width) / unit.tocm(name.bbox().width())
-        c.insert(name, [trafo.scale(namesmultiplier),
-                        trafo.translate(self.x(framesize+margin),
-                                        self.profile.height - self.y(framesize+margin+name_height))])
-        c.insert(handle)
-        c.insert(uni)
+        c.insert(name, [trafo.scale(getTextWidthMul(name, txt_width)),
+                        trafo.translate(framesize+margin,
+                                        self.profile.height - (framesize+margin+name_height))])
+        c.insert(handle, [trafo.scale(getTextWidthMul(handle, txt_width)),
+                          trafo.translate(framesize+margin,
+                                          self.profile.height - (framesize+margin+handle_pos))])
+        c.insert(uni, [trafo.scale(getTextWidthMul(uni, txt_width)),
+                       trafo.translate(framesize+margin,
+                                       self.profile.height - (framesize+margin+uni_pos))])
         c.insert(kif)
         if self.profile.qr:
             c.insert(qr)
@@ -157,19 +182,12 @@ class Badge:
         code.save(self.tmp_file)
 
 
-    def x(self, mm):
-        return (self.profile.width / 105) * mm
-
-
-    def y(self,mm):
-        return self.x(mm)
-
-
 def import_csv(filename, profile, count):
     pages = []
     with open(filename) as file:
         reader = csv.reader(file, delimiter=';', escapechar="\\", doublequote=False)
         for row in reader:
+            print_info("Parsing attandee: '" + row[0] + "'")
             badge = Badge(*row,profile)
             page = badge.draw()
             for i in range(count):
@@ -184,8 +202,9 @@ def import_csv(filename, profile, count):
 def print_err(str):
     print("\33[91m" + str + "\33[0m")
 
-def print_info(str):
-    print("\33[93m" + str + "\33[0m")
+def print_info(str, verbose = True):
+    if verbose:
+        print("\33[93m" + str + "\33[0m")
 
 def print_succ(str):
     print("\33[1m\33[92m" + str + "\33[0m")
